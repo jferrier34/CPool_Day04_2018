3cb1d0dc13ae0b5cfda7a0d37734a3069dfcfc37
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-04 21:51:33 +0200

    one

--- my_putstr.c
+++ my_putstr.c
@@ -2,17 +2,17 @@
 ** EPITECH PROJECT, 2018
 ** my_putstr.c
 ** File description:
-** Task04
+** Task02
 */
 
-int  my_putstr(char const *str){
+int  my_putstr(char const *str)
+{
     char i = *str;
 
     while (i != '\0'){
-    i = *str;
-    my_putchar(i);
-    str = str + 1;
+        i = *str;
+        my_putchar(i);
+        str = str + 1;
     }
     return (0);
 }
-
